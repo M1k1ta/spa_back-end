6c85aa35b11af9be616db6ec131434ff94715321
Mykyta Holtsov <nikitaholtsov@gmail.com>
2023-09-28 19:01:12 +0300

    last fixes

---
+++ README.md
@@ -0,0 +1,30 @@
+# spa_back-end
+
+[DEMO](https://spa-back-end-5cn9.onrender.com)
+
+GET
+  [/messages](https://spa-back-end-5cn9.onrender.com/messages)
+
+  [/messages?order=desc&sort=email&page=1](https://spa-back-end-5cn9.onrender.com/messages?order=desc&sort=email&page=1)
+
+  [/files/1](https://spa-back-end-5cn9.onrender.com/files/1)
+
+POST
+  /messages need send {
+    userName,
+    email,
+    homePage,
+    editorState,
+    photos,
+    docs,
+    relatedId,
+  }
+
+  /messages return newMessage
+
+  /files send data = new FormData() in FormData array files
+  
+  /files return {
+    photosLinks: [],
+    docsLinks: [],
+  }
--- src/utils/groupMessagesByConversations.ts
+++ src/utils/groupMessagesByConversations.ts
@@ -4,15 +4,6 @@ interface Conversation extends Message {
   messages?: Message[],
 }
 
-// interface M {
-//   id: number,
-//   text: string,
-//   userName: string,
-//   email: string,
-//   relatedId: number | null,
-//   messages?: M[],
-// }
-
 export const groupMessagesByConversations = (data: Message[]) => {
   const messageList: Conversation[] = JSON.parse(JSON.stringify(data));
   const conversationList: Conversation[] = [];
@@ -31,78 +22,3 @@ export const groupMessagesByConversations = (data: Message[]) => {
 
   return conversationList;
 };
-
-// const groupMessagesByConversations1 = (data: M[]) => {
-//   const messageMap = new Map();
-//   const conversations: M[] = [];
-
-//   data.forEach((message) => {
-//     if (message.relatedId === null) {
-//       conversations.push(message);
-//     } else {
-//       if (!messageMap.has(message.relatedId)) {
-//         messageMap.set(message.relatedId, []);
-//       }
-//       messageMap.get(message.relatedId).push(message);
-//     }
-//   });
-
-//   conversations.forEach((conversation) => {
-//     if (messageMap.has(conversation.id)) {
-//       conversation.messages = messageMap.get(conversation.id);
-//     }
-//   });
-
-//   return conversations;
-// };
-
-
-// const arr: M[] = [
-//   {
-//     id: 1,
-//     userName: 'rtyu',
-//     text: '11111',
-//     email: '1111',
-//     relatedId: null,
-//   },
-//   {
-//     id: 2,
-//     userName: 'rtyu',
-//     text: '11111',
-//     email: '1111',
-//     relatedId: 1,
-//   },
-//   {
-//     id: 3,
-//     userName: 'rtyu',
-//     text: '11111',
-//     email: '1111',
-//     relatedId: 1,
-//   },
-//   {
-//     id: 4,
-//     userName: 'rtyu',
-//     text: '11111',
-//     email: '1111',
-//     relatedId: 2,
-//   },
-//   {
-//     id: 5,
-//     userName: 'rtyu',
-//     text: '11111',
-//     email: '1111',
-//     relatedId: null,
-//   },
-//   {
-//     id: 6,
-//     userName: 'rtyu',
-//     text: '11111',
-//     email: '1111',
-//     relatedId: 3,
-//   },
-// ];
-
-// const result = groupMessagesByConversations1(arr);
-// console.log(JSON.stringify(result, null, 2));
-// console.log(count);
-
